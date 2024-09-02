42aa44dd21f45e76594d1948c7dccca2ff4372d2
Jonas <loschag@outlook.de>
2024-09-02 17:10:45 +0200

    Switch to standard Dassie error messages

--- src/Dassie.Preprocessor/Errors/ErrorWriter.cs
+++ src/Dassie.Preprocessor/Errors/ErrorWriter.cs
@@ -1,5 +1,7 @@
 ﻿global using static Dassie.Preprocessor.Errors.ErrorWriter;
+using DSErrorWriter = Dassie.Errors.ErrorWriter;
 using System;
+using Dassie.Errors;
 
 namespace Dassie.Preprocessor.Errors;
 
@@ -7,12 +9,15 @@ internal static class ErrorWriter
 {
     public static void EmitError(string message, (int Line, int Column) pos = default, string file = null)
     {
-        if (!string.IsNullOrEmpty(file))
-            Console.Error.Write($"{file}");
-
-        if (pos != default)
-            Console.Error.Write($"({pos.Line},{pos.Column}): ");
-
-        Console.WriteLine($"Error: {message}");
+        DSErrorWriter.EmitErrorMessage(new ErrorInfo()
+        {
+            CodePosition = pos,
+            ErrorMessage = message,
+            ErrorCode = ErrorKind.CustomError,
+            CustomErrorCode = "DSTT01",
+            Severity = Severity.Error,
+            File = file,
+            Length = 1
+        });
     }
 }

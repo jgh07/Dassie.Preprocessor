ddbdf4083f8f4f04e5ae60e09437485b757b1ac6
Jonas <loschag@outlook.de>
2024-09-02 17:19:14 +0200

    Move PrintUsage() to PreprocessCommand

--- src/Dassie.Preprocessor/PreprocessCommand.cs
+++ src/Dassie.Preprocessor/PreprocessCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Dassie.Extensions;
 using System;
 using System.IO;
+using System.Linq;
 
 namespace Dassie.Preprocessor;
 
@@ -14,10 +15,19 @@ public class PreprocessCommand : ICompilerCommand
 
     public int Invoke(string[] args)
     {
-        if (args == null || args.Length == 0)
+        static void PrintUsage()
         {
-            Console.WriteLine("Error: No input file specified.");
-            return -1;
+            Console.WriteLine("Dassie Preprocessor");
+            Console.WriteLine("Version 1.0, (C) 2024 Jonas H.");
+            Console.WriteLine();
+
+            Console.WriteLine("Usage: pp <InputFile> [OutputFile]");
+        }
+
+        if (args == null || args.Length == 0 || ((string[])["help", "?", "--help", "-h", "-?", "/?", "/help", "/h"]).Contains(args[0]))
+        {
+            PrintUsage();
+            return 0;
         }
 
         string inFile = Path.GetFullPath(args[0]);
--- src/Dassie.Preprocessor/Program.cs
+++ src/Dassie.Preprocessor/Program.cs
@@ -1,21 +1,4 @@
 ﻿using Dassie.Preprocessor;
-using System;
-using System.Linq;
-
-static void PrintUsage()
-{
-    Console.WriteLine("Dassie Preprocessor");
-    Console.WriteLine("Version 1.0, (C) 2024 Jonas H.");
-    Console.WriteLine();
-
-    Console.WriteLine("Usage: pp <InputFile> [OutputFile]");
-}
-
-if (args == null || args.Length == 0 || ((string[])["help", "?", "--help", "-h", "-?", "/?", "/help", "/h"]).Contains(args[0]))
-{
-    PrintUsage();
-    return;
-}
 
 PreprocessCommand cmd = new();
 cmd.Invoke(args);
